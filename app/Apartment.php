27026c23a48950e8f659e18021d882269c90851b
leitong <leitongchen93@gmail.com>
2021-07-20 10:17:57 +0200

    Adds relation user - apartments

--- app/Apartment.php
+++ app/Apartment.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+
 
 class Apartment extends Model
 {
     protected $fillable = ['title', 'user_id', 'address_street', 'street_number', 'city', 'zip_code', 'province', 'nation', 'latitude', 'longitude', 'rooms_number', 'beds_number', 'bathrooms_number', 'floor_area', 'img_url', 'visible'];
+
+    public function user() 
+    {
+        return $this->belongsTo('App\User');
+    }
 }
