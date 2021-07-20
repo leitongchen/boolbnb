0e73e8245583b5db304915ef5a979938c70accf3
Polidora90 <alessandra.torry@gmail.com>
2021-07-20 10:14:04 +0200

    adds foreign key and fillable to apartments

--- app/Apartment.php
+++ app/Apartment.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    //
+    protected $fillable = ['title', 'user_id', 'address_street', 'street_number', 'city', 'zip_code', 'province', 'nation', 'latitude', 'longitude', 'rooms_number', 'beds_number', 'bathrooms_number', 'floor_area', 'img_url', 'visible'];
 }
