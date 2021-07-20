11f3e29f7e4ff2e7fa15d534f569b21965059c78
leitong <leitongchen93@gmail.com>
2021-07-20 12:05:49 +0200

    adds sponsorship table and pivot table with relations

--- app/Apartment.php
+++ app/Apartment.php
@@ -24,4 +24,9 @@ class Apartment extends Model
     {
         return $this->hasMany('App\Message');
     }
+
+    public function sponsorships()
+    {
+        return $this->belongsToMany('App\Sponsorship');
+    }
 }
---
+++ database/migrations/2021_07_20_094900_create_apartment_sponsorship_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentSponsorshipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+
+            $table->date('start_at');
+
+            $table->foreignId('apartment_id')
+            ->constrained();
+        
+            $table->foreignId('sponsorship_id')
+            ->constrained();
+
+            $table->primary(["apartment_id", "sponsorship_id"]);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+}
