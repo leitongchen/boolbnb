d7da9198c14e888101f3fd55745a89f1a5cfe9d4
Ciarm-nik <nikcia@libero.it>
2021-07-23 16:21:27 +0200

    Add container with bootstrap

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -2,35 +2,42 @@
 
 @section('content')
 
+<div class="container">
+
     <h1>DETTAGLI APPARTAMENTO (PRIVATO)</h1>
 
-    <h3>{{ $apartment->title }}<h3>
-    <ul>
-        <li>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</li>
-        <li>Latitudine: {{ $apartment->latitude }}</li>
-        <li>Longitudine: {{ $apartment->longitude }}</li>
-        <li>Locali: {{ $apartment->rooms_number }}</li>
-        <li>Posti letto: {{ $apartment->beds_number }}</li>
-        <li>Bagni: {{ $apartment->bathrooms_number }}</li>
-        <li>Metratura: {{ $apartment->floor_area . ' mq' }}</li>
-        <li>
-            <img src="{{ $apartment->img_url }}" alt="">
-        </li>
-        <li>{{ $apartment->visible }}</li>
-    </ul>
+    <h3>{{ $apartment->title }}
+        <h3>
+            <ul>
+                <li>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</li>
+                <li>Latitudine: {{ $apartment->latitude }}</li>
+                <li>Longitudine: {{ $apartment->longitude }}</li>
+                <li>Locali: {{ $apartment->rooms_number }}</li>
+                <li>Posti letto: {{ $apartment->beds_number }}</li>
+                <li>Bagni: {{ $apartment->bathrooms_number }}</li>
+                <li>Metratura: {{ $apartment->floor_area . ' mq' }}</li>
+                <li>
+                    <img src="{{ $apartment->img_url }}" alt="">
+                </li>
+                <li>{{ $apartment->visible }}</li>
+            </ul>
+
+            <button class="btn btn-warning"> <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}">MODIFICA</a> <br></button>
+
+            <a href="{{ route('admin.apartments.index') }}">Torna a tutti gli appartamenti</a>
+
+            <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
+                @csrf
+
+                @method('DELETE')
+
+                <div class="form-group">
+                        <input class="btn btn-danger" type="submit" value="ELIMINA">
+                    </div>
+            </form>
 
 
-    <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}">MODIFICA</a> <br>
-    <a href="{{ route('admin.apartments.index') }}">Torna a tutti gli appartamenti</a>
+</div>
 
-    <form action="{{ route('admin.apartments.destroy', $apartment->id) }}"
-        method="POST">
-        @csrf
 
-        @method('DELETE')
-    
-        <input type="submit" value="ELIMINA">
-    </form>
-       
-    
 @endsection
