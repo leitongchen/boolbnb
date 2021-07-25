ad7f707c0125c5be04e52ac4677844439ee17b5d
Polidora90 <alessandra.torry@gmail.com>
2021-07-25 12:38:41 +0200

    fix relation with extra services

--- app/Apartment.php
+++ app/Apartment.php
@@ -8,7 +8,7 @@ use App\User;
 
 class Apartment extends Model
 {
-    protected $fillable = ['title', 'user_id', 'address_street', 'street_number', 'city', 'zip_code', 'province', 'nation', 'latitude', 'longitude', 'rooms_number', 'beds_number', 'bathrooms_number', 'floor_area', 'img_url', 'visible', 'extra_service_id'];
+    protected $fillable = ['title', 'user_id', 'address_street', 'street_number', 'city', 'zip_code', 'province', 'nation', 'latitude', 'longitude', 'rooms_number', 'beds_number', 'bathrooms_number', 'floor_area', 'img_url', 'visible', 'extra_services_id'];
 
     public function user() 
     {
@@ -17,7 +17,13 @@ class Apartment extends Model
 
     public function extra_services()
     {
-        return $this->belongsToMany('App\Extra_service');
+         //argometi per specificare le colonne che erano scritte sbagliatamente
+         return $this->belongsToMany(
+            'App\Extra_service',
+            'apartment_extra_service',
+            'apartment_id',
+            'extra_services_id'
+        );
     }
 
     public function messages() 
