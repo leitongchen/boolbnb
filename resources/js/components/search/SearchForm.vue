1d643d3df54e09208a6f584f72be84edcde8e42d
leitong <leitongchen93@gmail.com>
2021-07-25 11:29:24 +0200

    adds some comments of things to do; change on index apartments order by updated date

--- resources/js/components/search/SearchForm.vue
+++ resources/js/components/search/SearchForm.vue
@@ -117,7 +117,7 @@ import InputAtom from '../formInputs/InputAtom.vue';
 
                 auth_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNmE4NDU3OTc0MDUyMDViNzZhMzhmMzFlZTcwYzVjODliNmUzMDMyYmRkODJjOGJjOTM3ZDg1ZjUzMjQyY2M0Y2ViZDgxNjQxYmQ5MTc4YTYiLCJpYXQiOjE2MjcxOTgxMTQuNTk0NTkyLCJuYmYiOjE2MjcxOTgxMTQuNTk0NjA3LCJleHAiOjE2NTg3MzQxMTQuNTcxNDYyLCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.p-RjR6BmklJGosGbYy1B3zDF3eDnMOddKNalJSw5Xfrxo3maqQFK9SUVL60-RaMt1Gcjcl4XOTA7ta29lYkASN3yZrmo7EG_Vn_BvyZZu7rp3gofegeQBgvB1JcMZTeSYVxGjyq2WL0Q3nV-PENfL1OcDSClLCR0_M_cOc4HorgBnvk3D5uRZZlbg6j602cmOWhDCa4axB4Sa-X_1DsBHc6ejKeYCEKdsGps7jNt2uKidSA9Hdo38-NscChCuh60jNghkeyDqwasGXQVPGVqE8-vQRImd4faqyx0t0imosnewQ5KKbmGp4WObuj-qWeVh9xipVjLffxs9Yruy8xHT_VrxJLCOrjSiXnJlR0_tqnbs1SuvXITfL_40xSNf8-BYzsWMPZJsstnDNpP5HKyZRxWrXpNt-koqZ-A8GDS1ZivXGo-tQQjtqXb8vktPF1t4fwwvDZdyaGG0UTO8Mt4b1PClo0JxdGvZq5qSk4Bgwa7cFbsXJ_fim5I-leX7u1p2adJJC18Nj1bpAK8KjOmwZYYPLMYgtATZ241NaO2lpHwZILAqWKZU765yg4B760rg8VpwEy40IYwJzDiZuU1XY_F6BPWAeuTegZR0Lm5aJMfvPUi-u_8Tn-HOA5HTi50rHydeg9t3-X588YLRhEifp7JWzXolHUgnl48vUPWHD0",
             
-                apartmentData: [],
+                allApartmentsData: [],
             }
         },
         methods: {
@@ -163,6 +163,21 @@ import InputAtom from '../formInputs/InputAtom.vue';
 
                 return valueArr;
             }
+
+            
+            // for every apartment (object)
+            // we needs to also save latitude and longitude fields (already created but set to null)
+            // First: we need to format the address data to obtain only the values
+                // - address_street 
+                // - street_number 
+                // - city 
+                // - zip_code 
+                // - province 
+                // - nation
+            // Second: we pass the value through the search function and handle the json result
+                // where we can find lat and lng values of the specific address
+            // Third: we save lat and lng values for each apartment of the array
+            // Fourth: the data needs to be sent to backend in order to be stored
         },
         mounted() {
 
@@ -173,7 +188,7 @@ import InputAtom from '../formInputs/InputAtom.vue';
                 }
             })
             .then(resp => {
-                console.log(resp);
+                this.allApartmentsData = resp.data.results;
             })
             .catch(er => {
                 console.log(er);
