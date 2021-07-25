957d8177547723e953a1ea8ff7f6ba7e54ce86bd
leitong <leitongchen93@gmail.com>
2021-07-25 13:18:44 +0200

    Save Lat & Lng param to apartment

--- resources/js/components/search/SearchForm.vue
+++ resources/js/components/search/SearchForm.vue
@@ -101,9 +101,12 @@
 </template>
 
 <script>
-import InputAtom from '../formInputs/InputAtom.vue';
+    import InputAtom from '../formInputs/InputAtom.vue';
+
     export default {
-  components: { InputAtom },
+    components: { 
+        InputAtom 
+        },
         name: "SearchForm",
         props: {
         
@@ -118,6 +121,7 @@ import InputAtom from '../formInputs/InputAtom.vue';
                 auth_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNmE4NDU3OTc0MDUyMDViNzZhMzhmMzFlZTcwYzVjODliNmUzMDMyYmRkODJjOGJjOTM3ZDg1ZjUzMjQyY2M0Y2ViZDgxNjQxYmQ5MTc4YTYiLCJpYXQiOjE2MjcxOTgxMTQuNTk0NTkyLCJuYmYiOjE2MjcxOTgxMTQuNTk0NjA3LCJleHAiOjE2NTg3MzQxMTQuNTcxNDYyLCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.p-RjR6BmklJGosGbYy1B3zDF3eDnMOddKNalJSw5Xfrxo3maqQFK9SUVL60-RaMt1Gcjcl4XOTA7ta29lYkASN3yZrmo7EG_Vn_BvyZZu7rp3gofegeQBgvB1JcMZTeSYVxGjyq2WL0Q3nV-PENfL1OcDSClLCR0_M_cOc4HorgBnvk3D5uRZZlbg6j602cmOWhDCa4axB4Sa-X_1DsBHc6ejKeYCEKdsGps7jNt2uKidSA9Hdo38-NscChCuh60jNghkeyDqwasGXQVPGVqE8-vQRImd4faqyx0t0imosnewQ5KKbmGp4WObuj-qWeVh9xipVjLffxs9Yruy8xHT_VrxJLCOrjSiXnJlR0_tqnbs1SuvXITfL_40xSNf8-BYzsWMPZJsstnDNpP5HKyZRxWrXpNt-koqZ-A8GDS1ZivXGo-tQQjtqXb8vktPF1t4fwwvDZdyaGG0UTO8Mt4b1PClo0JxdGvZq5qSk4Bgwa7cFbsXJ_fim5I-leX7u1p2adJJC18Nj1bpAK8KjOmwZYYPLMYgtATZ241NaO2lpHwZILAqWKZU765yg4B760rg8VpwEy40IYwJzDiZuU1XY_F6BPWAeuTegZR0Lm5aJMfvPUi-u_8Tn-HOA5HTi50rHydeg9t3-X588YLRhEifp7JWzXolHUgnl48vUPWHD0",
             
                 allApartmentsData: [],
+                apartmentData: null,
             }
         },
         methods: {
@@ -154,20 +158,12 @@ import InputAtom from '../formInputs/InputAtom.vue';
 
                 }).go().then(this.handleResults);
             },
-
-            clearQuery(...data) {
-                // console.log(data)
-
-                let valueArr = (Object.values(data[0])).join(', ');
-                // console.log(valueArr);
-
-                return valueArr;
-            }
-
             
+
+
             // for every apartment (object)
-            // we needs to also save latitude and longitude fields (already created but set to null)
-            // First: we need to format the address data to obtain only the values
+            // we need to also save latitude and longitude fields (already created but set to null)
+            // First: we need to format the address data to obtain only the values of in a String
                 // - address_street 
                 // - street_number 
                 // - city 
@@ -178,8 +174,73 @@ import InputAtom from '../formInputs/InputAtom.vue';
                 // where we can find lat and lng values of the specific address
             // Third: we save lat and lng values for each apartment of the array
             // Fourth: the data needs to be sent to backend in order to be stored
+
+
+            clearQuery(addressObj) {
+
+                let valueArr = (Object.values(addressObj)).join(', ');
+                // console.log(valueArr);
+
+                return valueArr;
+            },
+
+            getLatLng() {
+                
+                const el = this.allApartmentsData[0];
+
+                let completeAddress = {
+                    address_street: el.address_street,
+                    street_number: el.street_number,
+                    city: el.city,
+                    zip_code: el.zip_code,
+                    province: el.province,
+                    nation: el.nation,
+                }  
+                
+                this.apartmentData = el; 
+                
+                let addressStr = this.clearQuery(completeAddress);
+                this.searchApartment(addressStr);          
+            },
+
+            setLatLng(position) {
+                // console.log(position);
+                this.apartmentData.latitude = position.lat;
+                this.apartmentData.longitude = position.lng;
+                // console.log(this.apartmentData)
+            },
+
+            searchApartment(query) {
+
+                tt.services.fuzzySearch({
+                    key: this.api_key,
+                    query: query,
+                    // boundingBox: map.getBounds()
+
+                }).go().then(resp => {
+                    const position = resp.results[0].position; 
+                    this.setLatLng(position);
+                })
+                .catch(er => {
+                    console.log(er);
+                });
+            },
+
+            //HTTP request to TOMTOM API
+            // getLatLng(addressStr) {
+            //     // Http request get TOMTOM
+            //     axios.get(`https://api.tomtom.com/search/2/search/${addressStr}.json?key=${this.api_key}`)
+            //         .then(resp => {
+            //             console.log(resp);
+            //         })
+            //         .catch(er => {
+            //             console.log(er);
+            //         })
+            // }
+
         },
         mounted() {
+            
 
             // API request for db apartments
             axios.get('http://127.0.0.1:8000/api/user/apartments', {
@@ -189,6 +250,7 @@ import InputAtom from '../formInputs/InputAtom.vue';
             })
             .then(resp => {
                 this.allApartmentsData = resp.data.results;
+                this.getLatLng();
             })
             .catch(er => {
                 console.log(er);
