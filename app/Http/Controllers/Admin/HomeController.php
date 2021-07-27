98805361b66e0bf2b7b6e359b879c13f76df48e9
Polidora90 <alessandra.torry@gmail.com>
2021-07-27 10:29:47 +0200

    prova

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Apartment;
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    public function index () {
-        return view ("admin.home",);
+    public function index (Request $request, Apartment $apartment) {
+
+        $userId = Auth::id();
+        $user = Auth::user();
+
+        // $apartments = $user->apartments()->get();
+        
+        
+        return view ("admin.home",[
+            
+        ]);
     }
 }
+
+// public function index(Request $request) {
+//     $statistics = [
+//         "posts" => Post::count(),
+//         "categories" =>Category::count(),
+//     ];
+
+//     return view("admin.home", [
+//         "statistics" => $statistics,
+//         "user" => $request->user(),
+//         "userDetails" => $request->user()->detail
+//         //"user" => Auth::user()
+//     ]);
+// }
