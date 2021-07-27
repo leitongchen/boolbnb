0b798c3234ccad1f2e90009c5953b4156d63f6d1
Polidora90 <alessandra.torry@gmail.com>
2021-07-27 12:02:16 +0200

    add comments

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -10,30 +10,9 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    public function index (Request $request, Apartment $apartment) {
+    public function index () {
 
-        $userId = Auth::id();
-        $user = Auth::user();
-
-        // $apartments = $user->apartments()->get();
-        
-        
-        return view ("admin.home",[
+        return view ("admin.home");
             
-        ]);
     }
 }
-
-// public function index(Request $request) {
-//     $statistics = [
-//         "posts" => Post::count(),
-//         "categories" =>Category::count(),
-//     ];
-
-//     return view("admin.home", [
-//         "statistics" => $statistics,
-//         "user" => $request->user(),
-//         "userDetails" => $request->user()->detail
-//         //"user" => Auth::user()
-//     ]);
-// }
