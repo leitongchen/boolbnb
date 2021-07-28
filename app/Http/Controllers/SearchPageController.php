43481b6a12c02598e01273a6cbc3fecfc988a3b8
leitong <leitongchen93@gmail.com>
2021-07-28 14:53:06 +0200

    bug fixed on searchPage now shows apartments always

--- app/Http/Controllers/SearchPageController.php
+++ app/Http/Controllers/SearchPageController.php
@@ -15,35 +15,34 @@ class SearchPageController extends Controller
      */
     public function index(Request $request)
     {
-        // $data = $request->all();
+        $data = $request->all();
 
         $apartments = [];
+        // $apartments = Apartment::orderBy('updated_at', 'DESC')->get();
 
-        if (!isset($request)) {
+        if (count($data) === 0) {
 
             $apartments = Apartment::orderBy('updated_at', 'DESC')->get();
+     
+        } else {
+
+            $latitude = $request->latitude;
+            $longitude = $request->longitude;
+    
+            // dd($request);
+            // return; 
+    
+            $km = 100;
+    
+            $apartments = Apartment::select(DB::raw("id, title, address_street, street_number, city, zip_code, province, nation, latitude, longitude, rooms_number, beds_number, bathrooms_number, floor_area, img_url, visible,
+            ( 6371 * acos( cos( radians('$latitude') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('$longitude') ) + sin( radians('$latitude') ) * sin( radians( latitude ) ) ) ) AS distance"))
+                ->havingRaw('distance <' . $km)
+                ->orderBy('distance')
+                ->get();         
+    
+            // dd($apartments);
+            // return; 
         }
-
-        // dd($data);
-        // dump($request->latitude);
-        // return; 
-
-        $latitude = $request->latitude;
-        $longitude = $request->longitude;
-
-        // dd($request);
-        // return; 
-
-        $km = 100;
-
-        $apartments = Apartment::select(DB::raw("id, title, address_street, street_number, city, zip_code, province, nation, latitude, longitude, rooms_number, beds_number, bathrooms_number, floor_area, img_url, visible,
-        ( 6371 * acos( cos( radians('$latitude') ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians('$longitude') ) + sin( radians('$latitude') ) * sin( radians( latitude ) ) ) ) AS distance"))
-            ->havingRaw('distance <' . $km)
-            ->orderBy('distance')
-            ->get();         
-
-        // dd($apartments);
-        // return; 
     
         return view('search.search', ['apartments' => $apartments]);
     }
