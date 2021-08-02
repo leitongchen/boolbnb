c7b0f5f61adbdda3519d93f40c56d569c9b29754
leitong <leitongchen93@gmail.com>
2021-08-02 18:22:52 +0200

    change db query x sponsored apartment, now based on end_at data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,41 +27,13 @@ class HomeController extends Controller
     public function index()
     {
         $now = Carbon::now()->toDateTimeString(); 
-        $start_search = Carbon::now()->subHours(144)->toDateTimeString();
 
-        // resultsList contain a list of apartments where had promotions in the last 144hours
-        $resultsList = Apartment::
+        $sponsoredApartments = Apartment::
                 join("apartment_sponsorship", "apartments.id", "=", "apartment_sponsorship.apartment_id")  
-                ->whereBetween('start_at', [$start_search, $now])
+                ->where('end_at', '>', $now)
                 ->with('sponsorships')
                 ->get(); 
 
-        // this array will contain all the apartments with still active promotion
-        // sponsored apartments will be pushed after saving end_at date and checked if isPast or not
-        $sponsoredApartments = [];
-                
-        foreach ($resultsList as $el) {
-            
-            $hours = $el->sponsorships[0]->promo_hours;
-
-            if ($hours == 24) {
-
-                $el->end_at = Carbon::createFromFormat('Y-m-d H:i:s', $el->start_at)->addHours(24);
-
-            } else if ($hours == 72) {
-
-                $el->end_at = Carbon::createFromFormat('Y-m-d H:i:s', $el->start_at)->addHours(72);
-
-            } else if ($hours == 144) {
-
-                $el->end_at = Carbon::createFromFormat('Y-m-d H:i:s', $el->start_at)->addHours(144);
-            }
-
-            if (!$el->end_at->isPast()) {
-
-                $sponsoredApartments[] = $el; 
-            }
-        }
 
         $data = [
             "sponsored" => $sponsoredApartments,
@@ -72,3 +44,18 @@ class HomeController extends Controller
         return view('home', $data);
     }
 }
+
+// $hours = $el->sponsorships[0]->promo_hours;
+
+// if ($hours == 24) {
+
+//     $el->end_at = Carbon::createFromFormat('Y-m-d H:i:s', $el->start_at)->addHours(24);
+
+// } else if ($hours == 72) {
+
+//     $el->end_at = Carbon::createFromFormat('Y-m-d H:i:s', $el->start_at)->addHours(72);
+
+// } else if ($hours == 144) {
+
+//     $el->end_at = Carbon::createFromFormat('Y-m-d H:i:s', $el->start_at)->addHours(144);
+// }
