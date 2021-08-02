b5709b624ee001d2dc44bda2aa07faf1a63883f8
Polidora90 <alessandra.torry@gmail.com>
2021-08-02 23:10:34 +0200

    adds map to show view

---
+++ resources/js/components/ApartmentShowMap.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <div>
+        <h1>L'appartamento si trova qui:</h1>
+
+        <div class='map' id='map' ref="mapRef"> TomTom map! </div>
+        <p>{{ this.apartment }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ApartmentShowMap",
+    props: ['apartment'], 
+
+    data() {
+        return {
+            // location: [],
+            map: {},
+            marker: []
+        }
+    },
+    mounted() {
+        this.map = tt.map({
+            key: "E8JECf2Pom9XoKTM5Gs0GGdIBDUOYbnS",
+            container: 'map',
+            center: [11.005780, 45.443520],
+            zoom:14
+        })
+
+        // var location = [(apartment.longitude, apartment.latitude)];
+        var HQ = { lat: 45.443520, lng: 11.005780 }
+
+        console.log(location);
+
+        this.marker = new tt.Marker()
+        .setLngLat(HQ)
+        .addTo(map);
+    }
+};
+</script>
+
+
+<style> 
+#map { 
+    height: 50vh; 
+    width: 50vw; 
+} 
+</style> 
--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -8,58 +8,64 @@
 
     <h3>{{ $apartment->title }}<h3>
 
-            <ul>
-                <li>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</li>
-                <li>Latitudine: {{ $apartment->latitude }}</li>
-                <li>Longitudine: {{ $apartment->longitude }}</li>
-                <li>Locali: {{ $apartment->rooms_number }}</li>
-                <li>Posti letto: {{ $apartment->beds_number }}</li>
-                <li>Bagni: {{ $apartment->bathrooms_number }}</li>
-                <li>Metratura: {{ $apartment->floor_area . ' mq' }}</li>
-                <li>
-                    <img src="{{ $apartment->img_url }}" alt="">
-                </li>
-                <li>{{ $apartment->visible }}</li>
+    <ul>
+        <li>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</li>
+        <li>Latitudine: {{ $apartment->latitude }}</li>
+        <li>Longitudine: {{ $apartment->longitude }}</li>
+        <li>Locali: {{ $apartment->rooms_number }}</li>
+        <li>Posti letto: {{ $apartment->beds_number }}</li>
+        <li>Bagni: {{ $apartment->bathrooms_number }}</li>
+        <li>Metratura: {{ $apartment->floor_area . ' mq' }}</li>
+        <li>
+            <img src="{{ $apartment->img_url }}" alt="">
+        </li>
+        <li>{{ $apartment->visible }}</li>
 
 
-                {{-- @if (count($apartment->extra_services > 0)) --}}
-                <li>Servizi inclusi:</li>
-                <ul>
-                    @foreach($apartment->extra_services as $extraService)
-                    <li>
-                        {{ $extraService->name }}
-                    </li>
-                    @endforeach
-                </ul>
-                {{-- @endif --}}
-            </ul>
+        {{-- @if (count($apartment->extra_services > 0)) --}}
+        <li>Servizi inclusi:</li>
+        <ul>
+            @foreach($apartment->extra_services as $extraService)
+            <li>
+                {{ $extraService->name }}
+            </li>
+            @endforeach
+        </ul>
+        {{-- @endif --}}
+    </ul>
 
-            <button class="btn btn-warning"> <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}">MODIFICA</a> <br></button>
-
-
-
-            <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
-                @csrf
-
-                @method('DELETE')
-
-                <div class="form-group">
-                    <input class="btn btn-danger" type="submit" value="ELIMINA">
-                </div>
-            </form>
-
-            <a href="{{ route('admin.apartments.index') }}">Torna a tutti gli appartamenti</a>
-            <br>
-
-            @if ($userId == $apartment->user_id)
-            <a href="{{ route('admin.visits.show', $apartment->id) }}">Vedi le statistiche</a>
-            <br>
-            <a href="{{ route('admin.messages.index', $apartment->id) }}">Leggi i messaggi ricevuti</a> <br>
-            @endif
+    <button class="btn btn-warning"> <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}">MODIFICA</a> <br></button>
 
 
-            <a href="{{ route('messages.create', ['apartment' => $apartment->id]) }}">Manda un messaggio</a>
 
+    <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
+        @csrf
+
+        @method('DELETE')
+
+        <div class="form-group">
+            <input class="btn btn-danger" type="submit" value="ELIMINA">
+        </div>
+    </form>
+
+    <a href="{{ route('admin.apartments.index') }}">Torna a tutti gli appartamenti</a>
+    <br>
+
+    @if ($userId == $apartment->user_id)
+    <a href="{{ route('admin.visits.show', $apartment->id) }}">Vedi le statistiche</a>
+    <br>
+    <a href="{{ route('admin.messages.index', $apartment->id) }}">Leggi i messaggi ricevuti</a> <br>
+    @endif
+
+
+    <a href="{{ route('messages.create', ['apartment' => $apartment->id]) }}">Manda un messaggio</a>
+
+    <div id="app">
+        <apartment-show-map
+        :apartment="{{$apartment}}">
+        </apartment-show-map>
+
+    </div>
 
 
 </div>
