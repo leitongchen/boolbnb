936ace5930f58e4aea7ac31a0bcd6fb76d0fe488
SimonePerucino <simone.perucino@gmail.com>
2021-08-03 16:47:28 +0200

    Modifiche varie alla homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,53 +4,75 @@
 @include('partials.head')
 
 <body>
-    <div class="container-fluid">
-        <section id="section-1">
-            <div class="container">
-                <div class="row">
-                    <div class="col-2">
-                        <div>logo</div>
-                    </div>
-                    <div class="off"></div>
-                    <div class="col-2 offset-8">
-                        <div class="buttons_container">
+    <section id="section-1">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+
+                    {{-- lasciare questo ul separa il logo dai button --}}
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
+
+                    <span class="navbar-text">
+                        <div class="buttons_container display_flex justify_content_sa">
                             @if (Route::has('login'))
                             @auth
                             <a href="{{ url('/admin') }}">Home</a>
                             @else
-                            <button type="button" class="orange-button">
-                                <a href="{{ route('register') }}">Registrati</a>
-                            </button>
+                            <div class="white_button margin_r_15">
+                                <a href="{{ route('register') }}"><strong>Registrati</strong></a>
+                            </div>
                             @if (Route::has('register'))
-                            <button type="button" class="">
-                                <a href="{{ route('login') }}" class="">Accedi</a>
-                            </button>
+                            <div class="orange_button">
+                                <a href="{{ route('login') }}" class=""><strong>Accedi</strong></a>
+                            </div>
                             @endif
                             @endauth
                         </div>
                         @endif
-                    </div>
+                    </span>
                 </div>
-            </div>
-            <div class="content col-8">
-                <h1>
-                    stai pianificando un viaggio? <br>
-                    parti da qui!
+            </nav>
+            <div class="padding_t_400 padding_l_16">
+                <h1 class="text_color_w">
+                    <strong>
+                        Stai pianificando un viaggio? <br>
+                        parti da qui!
+                    </strong>
                 </h1>
             </div>
-            <div class="search-container col-6">
-                <div id="app">
-                    <search-input>
-                    </search-input>
-                </div>
+        </div>
+        <div>
+        nero sfumato
+        </div>
+    </section>
+    <section id="section-2">
+        <div class="search-container col-6 display_flex justify_content_ce">
+            <div id="app">
+                <h5 class="text_color_two margin_t_20 text-center"><strong>Qual è la tua prossima destinazione?</strong></h5>
+                <search-input>
+                </search-input>
             </div>
-        </section>
-        <section id="section-2">
-            <div class="links">
-                <a href="{{ route('apartments.index') }}">INDEX APPARTAMENTI PUBBLICO</a>
+        </div>
+        <h2 class="text-center text_color_two">In evidenza</h2>
+        <h2 class="text-center text_color_two">Più Recenti</h2>
+
+        <div class="links">
+            <a href="{{ route('apartments.index') }}">INDEX APPARTAMENTI PUBBLICO</a>
+        </div>
+    </section>
+    <section id="section-3">
+        <div class="container text-center">
+            <h1 class="text_color_w "><strong>Registrati subito e diventa un host!</strong></h1>
+            <div class="extra_button d-inline">
+                <a href="{{ route('register') }}"><strong>Registrati ora!</strong></a>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
     @include('partials.scripts')
 </body>
 </html>
