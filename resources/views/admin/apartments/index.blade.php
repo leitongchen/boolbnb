fd67f15943dec017bbacf1669490abdf649c4955
Polidora90 <alessandra.torry@gmail.com>
2021-08-04 01:43:57 +0200

    adds apartments cards WIP

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -3,54 +3,89 @@
 @section('content')
 
 <div class="container">
-    <h1>TUTTI GLI APPARTAMENTI (PRIVATO)</h1>
+    <div class="apartment-index-container">
+        <h1>I tuoi appartamenti</h1>
 
-    <button class="btn btn-secondary"><a href="{{ route('admin.apartments.create') }}">Aggiungi un nuovo appartamento</a></button>
-
-
-    @foreach ($apartments as $apartment)
-    <h3>{{ $apartment->title }}
-        <h3>
-            <ul>
-                <li>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</li>
-                <li>Latitudine: {{ $apartment->latitude }}</li>
-                <li>Longitudine: {{ $apartment->longitude }}</li>
-                <li>Locali: {{ $apartment->rooms_number }}</li>
-                <li>Posti letto: {{ $apartment->beds_number }}</li>
-                <li>Bagni: {{ $apartment->bathrooms_number }}</li>
-                <li>Metratura: {{ $apartment->floor_area . ' mq' }}</li>
-                <li>
-                    {{-- <img src="{{ $apartment->img_url }}" alt=""> --}}
-                    <img src="{{ asset('storage/' . $apartment->img_url) }}">
-
-                </li>
-            
-                <li>{{ $apartment->visible }}</li>
-                <li><a href="{{ URL::signedRoute('admin.apartments.show', $apartment->id) }}">Vedi i dettagli</a></li>
-                <li><a href="{{ URL::signedRoute('admin.messages.index', $apartment->id) }}">Leggi i messaggi ricevuti</a></li>
+        <button class="btn btn-secondary"><a href="{{ route('admin.apartments.create') }}">Aggiungi un nuovo appartamento</a></button>
 
 
-                <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
-                    @csrf
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 card-group">
+            @foreach ($apartments as $apartment)
+            <div class="col">
+                <div class="card my-apartment-card overflow-hidden h-100">
+                    <img src="{{ asset('storage/' . $apartment->img_url) }}" class="card-img-top my-card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $apartment->title }}</h5>
 
-                    @method('DELETE')
-                    <div class="form-group">
-                        <input class="btn btn-danger" type="submit" value="ELIMINA">
+                        @if ($apartment->rooms_number = 1) 
+                            <span>{{ $apartment->rooms_number }} stanza - </span>
+                        @else 
+                            <span>{{ $apartment->rooms_number }} stanze - </span>
+                        @endif
+
+                        @if ($apartment->beds_number = 1) 
+                            <span>{{ $apartment->beds_number }} letto - </span>
+                        @else 
+                            <span>{{ $apartment->beds_number }} letti - </span>
+                        @endif
+
+                        @if ($apartment->bathrooms_number = 1) 
+                            <span>{{ $apartment->bathrooms_number }} bagno</span>
+                        @else 
+                            <span>{{ $apartment->bathrooms_number }} bagni</span>
+                        @endif
+
+                        @if ($apartment->extra_services->count() > 0)
+                        <p class="card-text">
+                            @foreach($apartment->extra_services as $extraService)
+                            <span>{{ $extraService->name }}
+                                @if(!$loop->last)
+                                -
+                                @endif
+                            </span>
+                            @endforeach
+                        </p>
+                        @endif
+
+                        <a href="{{ URL::signedRoute('admin.apartments.show', $apartment->id) }}" class="white_button">Vedi i dettagli</a>
                     </div>
-
-                </form>
-            </ul>
-            @endforeach
-
-            <div class="card text-center" style="width: 18rem;">
-                <img src="{{ asset('storage/' . $apartment->img_url) }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h3>{{ $apartment->title }}
-                        <h3>
-                            <p>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</p>
-                            <button class="text-center btn btn-secondary"><a href="{{ route('admin.apartments.show', $apartment->id) }}">Vedi i dettagli</a></button>
                 </div>
             </div>
+
+            {{-- <h3>{{ $apartment->title }}
+            <h3>
+                <ul>
+                    <li>{{ $apartment->address_street . ' N° ' . $apartment->street_number . ', ' . $apartment->city . ', ' . $apartment->zip_code . ', ' . $apartment->nation }}</li>
+                    <li>Latitudine: {{ $apartment->latitude }}</li>
+                    <li>Longitudine: {{ $apartment->longitude }}</li>
+                    <li>Locali: {{ $apartment->rooms_number }}</li>
+                    <li>Posti letto: {{ $apartment->beds_number }}</li>
+                    <li>Bagni: {{ $apartment->bathrooms_number }}</li>
+                    <li>Metratura: {{ $apartment->floor_area . ' mq' }}</li>
+                    <li>
+                        <img src="{{ asset('storage/' . $apartment->img_url) }}">
+
+                    </li>
+                
+                    <li>{{ $apartment->visible }}</li>
+                    <li><a href="{{ URL::signedRoute('admin.apartments.show', $apartment->id) }}">Vedi i dettagli</a></li>
+                    <li><a href="{{ URL::signedRoute('admin.messages.index', $apartment->id) }}">Leggi i messaggi ricevuti</a></li>
+
+
+                    <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST">
+                        @csrf
+
+                        @method('DELETE')
+                        <div class="form-group">
+                            <input class="btn btn-danger" type="submit" value="ELIMINA">
+                        </div>
+
+                    </form>
+                </ul>--}}
+                @endforeach
+        </div>
+
+    </div>
 </div>
 
 @endsection
