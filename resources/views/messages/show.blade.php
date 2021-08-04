4b275c5aeb216b7a77584b0364863ee5d42e9503
Polidora90 <alessandra.torry@gmail.com>
2021-08-04 17:06:19 +0200

    adds data to message show view

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -2,8 +2,24 @@
 
 @section('content')
     
-<div class="container">
-    <h2>Il tuo messaggio è stato inviato!</h2>
+<div class="container show-msg-container">
+    <h1>Il tuo messaggio è stato inviato!</h1>
+
+    <div class="d-flex justify-content-between">
+        <div class="d-flex align-items-center">
+            <div class="my-avatar-container d-inline-block">
+                <img src="{{ asset('images/undraw_male_avatar_323b.svg') }}" class="img-fluid" alt="user avatar">
+            </div>
+            <div>
+                <span>da: <strong>{{ $messages->sender_name . " " . $messages->sender_surname}}</strong></span> <br>
+                <span>a: {{ $apartment->user->name . " " . $apartment->user->surname }}</span>
+            </div>
+        </div>
+
+        <div>
+            <span>{{ $carbonDate }}</span>
+        </div>
+    </div>
 
     <ul>
         <li>nome:{{$messages->sender_name}}</li>
