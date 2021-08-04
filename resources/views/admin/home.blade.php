0d6c45ab6e479b8285be66f779234612b512e9af
Polidora90 <alessandra.torry@gmail.com>
2021-08-04 15:33:04 +0200

    add img

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,8 +2,45 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center">
+    <div class="container my-dashboard-container">
+
+        <div class="card mb-3 mt-3" style="max-width: 100%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <div class="my-user-pic-container m-auto">
+                        <img src="{{ asset('images/undraw_Playful_cat_re_bxiu.svg') }}" class="img-fluid my-img-fluid" alt="...">
+                    </div>
+                    <div class="mb-3 text-center pt-1">
+                         @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+                        <span><strong>{{ __('Benvenuto '. Auth::user()->name . '!') }} </strong></span>
+                    </div>
+                    <div class="my-dashboard-list">
+                        <ul>
+                            <li><a href="{{ URL::signedRoute('admin.apartments.index') }}"><i class="fas fa-concierge-bell"></i>I tuoi appartamenti</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body d-flex justify-content-center my-card-body">
+
+                        <div class="my-apartments-info text-center">
+                            <h2 class="card-title">{{ count(Auth::user()->apartments) }}</h2>
+                            <p>Appartamenti <br> attivi </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        {{-- <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
@@ -24,7 +61,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 
 @endsection
